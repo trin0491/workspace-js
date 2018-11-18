df84dd75cc0b400d9cbc1b1e027e8dd351de7215
Richard Priestley <r.priestley@comanduk.co.uk>
2018-11-18 16:23:06 +0000

    Commented out external definitions in hydrogen

--- projects/hydrogen-widget/webpack.extra.js
+++ projects/hydrogen-widget/webpack.extra.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "externals": {
-    "rxjs": "rxjs",
-    "@angular/core": "ng.core",
-    "@angular/common": "ng.common",
-    "@angular/platform-browser": "ng.platformBrowser",
-    "@angular/platform-browser-dynamic": "ng.platformBrowserDynamic",
-    "@angular/elements": {
-      root: "ng.elements",
-      commonjs: "@angular/elements",
-      amd: '@angular/elements'
-    }
-  }
+  // "externals": {
+  //   "rxjs": "rxjs",
+  //   "@angular/core": "ng.core",
+  //   "@angular/common": "ng.common",
+  //   "@angular/platform-browser": "ng.platformBrowser",
+  //   "@angular/platform-browser-dynamic": "ng.platformBrowserDynamic",
+  //   "@angular/elements": {
+  //     root: "ng.elements",
+  //     commonjs: "@angular/elements",
+  //     amd: '@angular/elements'
+  //   }
+  // }
 };
